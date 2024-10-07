838015abde7446cb9544e780e9fb76dcf7746929
YerikZ <yerik.yilinzhu@gmail.com>
2024-10-07 09:45:06 +0700

    update index

--- config/index.ts
+++ config/index.ts
@@ -3,14 +3,16 @@ export const APP_ID = `${process.env.NEXT_PUBLIC_APP_ID}`
 export const API_KEY = `${process.env.NEXT_PUBLIC_APP_KEY}`
 export const API_URL = `${process.env.NEXT_PUBLIC_API_URL}`
 export const APP_INFO: AppInfo = {
-  title: 'Chat APP',
-  description: '',
-  copyright: '',
-  privacy_policy: '',
+  title: 'Efyia Resume',
+  description: `AI-powered resume builder and reviewer optimising your CV for job applications with personalised content suggestions. 
+                AI can make mistakes and fabricate content. Results should be reviewed to ensure accuracy and suitability.
+                Use at your own risk.`,
+  copyright: 'efyia.ai',
+  privacy_policy: 'https://dify.ai/privacy',
   default_language: 'en',
 }
 
-export const isShowPrompt = false
+export const isShowPrompt = true
 export const promptTemplate = 'I want you to act as a javascript console.'
 
 export const API_PREFIX = '/api'
